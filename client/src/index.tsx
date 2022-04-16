9a548c1d2bf43ebd466a2101320675440f7760fc
unrealisher96 <unrealisher96@yandex-team.ru>
2022-04-16 08:49:51 +0300

    initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import ProductList from './ProductList';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ProductList />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
