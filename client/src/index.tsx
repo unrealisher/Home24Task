5e994b6d766cbd39c8371f13853763688cbfe7eb
unrealisher96 <unrealisher96@yandex-team.ru>
2022-04-16 18:46:54 +0300

    Eslint

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,5 +8,5 @@ ReactDOM.render(
   <React.StrictMode>
     <ProductList />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
--- client/src/types.ts
+++ client/src/types.ts
@@ -3,31 +3,31 @@ export type Category = {
   categoryArticles: CategoryArticle
   articleCount: number
   childrenCategories: ChildCategory[]
-}
+};
 
 export type Article = {
   name:  string
   variantName: string
   prices: Prices
   images: Image[]
-}
+};
 
 export type ChildCategory = {
   name: string
   urlPath: string
-}
+};
 
 export type Prices = {
   currency: string
   regular: {
     value: number
   }
-}
+};
 
 export type Image = {
   path: string
-}
+};
 
 export type CategoryArticle = {
   articles: Article[]
-}
+};
